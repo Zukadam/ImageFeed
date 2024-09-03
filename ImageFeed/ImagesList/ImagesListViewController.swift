acd74ef3dfd861eb59193970e3fcc78cfb293daa
Cherkasov Vladimir <poisontermit90@gmail.com>
2024-09-04 00:13:37 +0300

    prepare for tableViewCell

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+
+    @IBOutlet private var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 200
+        
+    }
+    
+    private func configCell(for cell: ImagesListCell) {
+        
+    }
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let imagesListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        
+        configCell(for: imagesListCell)
+        
+        return imagesListCell
+    }
+    
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+}
