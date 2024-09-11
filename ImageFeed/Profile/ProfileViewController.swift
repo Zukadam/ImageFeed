d90ec2b1f9b16f8a124b2376ffdccfe01bd7af74
Cherkasov Vladimir <poisontermit90@gmail.com>
2024-09-11 15:57:04 +0300

    add profile screen

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton(_ sender: Any) {
+    }
+    
+    
+    
+}
