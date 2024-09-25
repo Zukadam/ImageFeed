048ecd05a9f948938c37ba0f1b4b686b4df9e7ed
Cherkasov Vladimir <poisontermit90@gmail.com>
2024-09-25 18:45:53 +0300

    add Constants

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "7Bl_LZ13dT9Jt_Jx5G21ljVUpR8tCgYwNS-PZb_Okbs"
+    static let secretKey = "0fHYczHXUAUUUdxb8EfJCYkmAgjKSF8MtWIVefyKTyQ"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
